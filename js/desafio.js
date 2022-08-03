c5538b9fa105722d0498edf0751135b46762edbb
ivana.cuello <moda.atica@gmail.com>
2022-08-02 21:20:35 -0300

    PrimeraVersion

---
+++ js/desafio.js
@@ -0,0 +1,30 @@
+
+
+
+//let limite = parseInt (prompt("Ingrese :"))
+
+//for (let i = 1; i <=7; i++) {
+//    console.log ("turno nº" , i)}
+
+debugger
+
+let Bienvenida = alert("Bienvenida, que lindo verte por aca!!")
+
+let compra = confirm("¿Deseas confirmar tu compra?") 
+
+let remera = confirm ("¿Llevas 1 remera por $1300?")
+let pantalon = confirm("¿Llevas un pantalon por $5200?")
+let campera = confirm("¿Llevas una campera por $12000?")
+
+
+sumar (1300, 5200,12000)
+
+function sumar() {
+    return 1300 + 5200 + 12000;
+}
+
+let resultado = sumar (1300, 5200, 12000)
+    console.log (resultado)
+    
+
+       
